878d66ee57979dbde2c1567a5c98389e7e08821e
Lion Rayonnant <106342136+lionrayonnant@users.noreply.github.com>
2026-01-15 00:03:59 +0100

    Adding segments and area method

--- point.py
+++ point.py
@@ -12,17 +12,20 @@ On testera la classe avec le point A(10,20)
 
 '''
 
+import math
+
 class Point:
-    def __init__(self,x,y):
+    def __init__(self,n,x,y):
+        self.name = n
         self.x = x
         self.y = y
         self.dist_xy = (x**2 + y**2)**0.5
 
     def __str__(self):
-        return f"x = {self.x} ; y = {self.y} ; distance xy : {self.dist_xy}\n"
+        return f"{self.name} ; x = {self.x} ; y = {self.y} ; distance xy : {self.dist_xy}\n"
     
-A = Point(10,20)
-print(f"A : {A}")
+#A = Point("A",10,20)
+#print(f"{A}")
 
 '''
 
@@ -34,6 +37,15 @@ On testera la classe avec les points A(1,1), B(2,4), C(6,9)
 
 '''
 
+'''
+
+On veut ajouter deux fonctions 
+▪Segments() : qui renvoie la taille de tous les segments
+▪Aire() : qui calcule l’aire du triangle
+On testera la classe avec les points A(1,1), B(2,4), C(6,9)
+
+'''
+
 class Triangle:
     def __init__(self,a,b,c):
         self.a = a
@@ -41,8 +53,33 @@ class Triangle:
         self.c = c
 
     def __str__(self):
-        return f"A : {self.a}B : {self.b}C : {self.c}"
-    
-tr = Triangle(Point(1,1),Point(2,4),Point(6,9))
-print(tr)
+        return f"{self.a}{self.b}{self.c}"
 
+    def segments(self, as_tuple=False):
+        def dist(p,q):
+            x = math.sqrt(((q.x - p.x)**2)+((q.y - p.y)**2))
+            return x
+        AB = dist(self.a, self.b)
+        BC = dist(self.b, self.c)
+        CA = dist(self.c, self.a)
+        if as_tuple:
+            return AB, BC, CA
+        else:
+            return f"{self.a.name}-{self.b.name} : {AB}\n{self.b.name}-{self.c.name} : {BC}\n{self.c.name}-{self.a.name} : {CA}"
+    
+    def aire(self):
+        AB, BC, CA = self.segments(as_tuple=True)
+        # utilisation de la formule de Héron
+        p = (AB + BC + CA)/2 # demi périmètre en cm
+        A = (math.sqrt(p*(AB)*(BC)*(CA))) # aire en cm^2
+        return f"{A}"
+    
+# Définition du triangle avec la classe Triangle
+tr = Triangle(Point("A",1,1),Point("B",2,4),Point("C",6,9))
+
+print(f"Coordonées du triangle :\n")
+print(tr)
+print(f"Segments du triangle :\n")
+print(tr.segments())
+print("Aire du triangle :\n")
+print(tr.aire())
